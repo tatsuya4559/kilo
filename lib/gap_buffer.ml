0d24430b91a7752721b5a0e6ddf909f837ff6b68
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-07-28 23:01:11 +0900

    format

--- lib/gap_buffer.ml
+++ lib/gap_buffer.ml
@@ -2,7 +2,6 @@
    whole = gapbuf gapbuf
    *)
 open Printf
-
 module Array = BatArray
 module Char = BatChar
 module IO = BatIO
@@ -16,13 +15,14 @@ end *)
 
 (* いったん char gapbufとして実装してからファンクタにする *)
 type t = {
-  mutable buf: char option array;
-  mutable gap_size: int;
-  mutable gap_offset: int;
+  mutable buf : char option array;
+  mutable gap_size : int;
+  mutable gap_offset : int;
 }
 
-(** debug print for t *)
 (* TODO: use ppx_derving.show *)
+
+(** debug print for t *)
 let debug t =
   let string_of_array = IO.to_string (Array.print (Option.print Char.print)) in
   sprintf "GapBuffer { buf = %s; gap_size = %d; gap_offset = %d; }"
@@ -32,15 +32,10 @@ let buf_equal = Array.equal (Option.eq ~eq:Char.equal)
 
 let equal t1 t2 =
   t1.gap_size = t2.gap_size
-    && t1.gap_offset = t2.gap_offset
-    && buf_equal t1.buf t2.buf
+  && t1.gap_offset = t2.gap_offset
+  && buf_equal t1.buf t2.buf
 
-let make size =
-  {
-    buf = Array.make size None;
-    gap_size = size;
-    gap_offset = 0;
-  }
+let make size = { buf = Array.make size None; gap_size = size; gap_offset = 0 }
 
 let at t offset =
   if offset < t.gap_offset then
@@ -53,41 +48,39 @@ let insert t offset elem =
   t.gap_size <- t.gap_size - 1;
   t.gap_offset <- t.gap_offset + 1
 
-let%test_module "gap_buffer test" = (module struct
+let%test_module "gap_buffer test" =
+  (module struct
+    let assert_buf_equal got want =
+      if equal got want then
+        ()
+      else
+        failwith
+          (sprintf "gap_buffer: want %s, got %s" (debug want) (debug got))
 
-  let assert_buf_equal got want =
-    if equal got want then
-      ()
-    else
-      failwith (sprintf "gap_buffer: want %s, got %s" (debug want) (debug got))
+    let%test_unit "create" =
+      let gapbuf = make 3 in
+      assert_buf_equal gapbuf
+        { buf = [| None; None; None |]; gap_size = 3; gap_offset = 0 }
 
+    let%test "get element before gap" =
+      let gapbuf = make 3 in
+      insert gapbuf 0 'a';
+      at gapbuf 0 = Some 'a'
 
-  let%test_unit "create" =
-    let gapbuf = make 3 in
-    assert_buf_equal gapbuf {
-      buf = [|None; None; None|];
-      gap_size = 3;
-      gap_offset = 0;
-    }
-
-  let%test "get element before gap" =
-    let gapbuf = make 3 in
-    insert gapbuf 0 'a';
-    at gapbuf 0 = Some 'a'
-
-  let%test_unit "insert at 0, 1" =
-    let gapbuf = make 5 in
-    insert gapbuf 0 'a';
-    assert_buf_equal gapbuf {
-      buf = [|Some 'a'; None; None; None; None|];
-      gap_size = 4;
-      gap_offset = 1;
-    };
-    insert gapbuf 1 'b';
-    assert_buf_equal gapbuf {
-      buf = [|Some 'a'; Some 'b'; None; None; None|];
-      gap_size = 3;
-      gap_offset = 2;
-    }
-
-end)
+    let%test_unit "insert at 0, 1" =
+      let gapbuf = make 5 in
+      insert gapbuf 0 'a';
+      assert_buf_equal gapbuf
+        {
+          buf = [| Some 'a'; None; None; None; None |];
+          gap_size = 4;
+          gap_offset = 1;
+        };
+      insert gapbuf 1 'b';
+      assert_buf_equal gapbuf
+        {
+          buf = [| Some 'a'; Some 'b'; None; None; None |];
+          gap_size = 3;
+          gap_offset = 2;
+        }
+  end)
